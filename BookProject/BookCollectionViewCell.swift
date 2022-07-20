2a812954e0c3353dd63996584838c5adc8d886eb
idhun90 <idhun90@naver.com>
2022-07-20 21:57:49 +0900

    Initial Commit

---
+++ BookProject/BookCollectionViewCell.swift
@@ -0,0 +1,10 @@
+
+import UIKit
+
+class BookCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var bookImageView: UIImageView!
+    @IBOutlet weak var bookNameLabel: UILabel!
+    @IBOutlet weak var rateLabel: UILabel!
+    
+}
---
+++ BookProject/BookCollectionViewController.swift
@@ -0,0 +1,18 @@
+
+import UIKit
+
+class BookCollectionViewController: UICollectionViewController {
+
+    @IBOutlet weak var contentView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+}
